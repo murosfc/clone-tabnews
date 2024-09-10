af0361f6c299799a84796ff935104fe2112ae4d6
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2024-09-10 21:52:52 +0000

    amended index file

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Você é a coisa mais importante na minha vida</h1>
+    return <h1>Nathália. Você é a coisa mais importante na minha vida</h1>
 }
 
 export default Home;
